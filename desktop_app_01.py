013832e67b9e0a2f4f2830a95613ed2ada19bc19
David2234504 <david2234504@correo.uis.edu.co>
2023-04-29 09:17:32 -0500

    se programó en python

--- desktop_app_01.py
+++ desktop_app_01.py
@@ -1,9 +1,37 @@
 # Se importa la libreria tkinter con todas las funciones
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------
 # Funciones de la app
 # ---------------------
+
+def calcular():
+    messagebox.showinfo("Minicalculadora1.0", "Hizo click en el botón calcular.")
+    s = int(a.get())+int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()}+{b.get()}={s}\n")
+    r = int(a.get())-int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()}-{b.get()}={r}\n")
+    m = int(a.get())*int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()}x{b.get()}={m}\n")
+    d = int(a.get())/int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()}/{b.get()}={d}\n")
+    de = int(a.get())//int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()}//{b.get()}={de}\n")
+    mod = int(a.get())%int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()}%{b.get()}={mod}\n")
+    p = int(a.get())**int(b.get())
+    t_resultados.insert(INSERT, f"{a.get()}^{b.get()}={p}\n")
+
+def borrar():
+    messagebox.showinfo("Minicalculadora1.0", "Los datos serán borrados.")
+    a.set("")
+    b.set("")
+    t_resultados.delete("1.0","end")
+
+def salir():
+    messagebox.showinfo("Minicalculadora1.0", "La app se cerrará")
+    ventana_principal.destroy()
 
 # ---------------------
 # Ventana principal
@@ -13,7 +41,7 @@ from tkinter import *
 ventana_principal = Tk()
 
 # titulo de la ventana
-ventana_principal.title("Sistemas UIS Socorro")
+ventana_principal.title("Minicalculadora01")
 
 # tamaño de la ventada
 ventana_principal.geometry("500x500")
@@ -22,28 +50,83 @@ ventana_principal.geometry("500x500")
 ventana_principal.resizable(0,0)
 
 # color de fondo de la ventana
-ventana_principal.config(bg="thistle4")
+ventana_principal.config(bg="gray")
+
+# ---------------------
+# Variables de valor
+# ---------------------
+
+a = StringVar()
+b = StringVar()
 
 # ---------------------
 # Frame entrada datos
 # ---------------------
 frame_entrada = Frame(ventana_principal)
-frame_entrada.config(bg="yellow",width=500, height=250)
-frame_entrada.place(x=0,y=0)
+frame_entrada.config(bg="white",width=480, height=180)
+frame_entrada.place(x=10,y=10)
 
+# Logo de la app
+logo = PhotoImage(file="img/logo_uis.png")
+lb_logo = Label(frame_entrada, image=logo, bg="white")
+lb_logo.place(x=11, y=40)
+
+# Etiqueta para el titulo
+lb_titulo = Label(frame_entrada, text ="Minicalculadora 1.0")
+lb_titulo.config(bg="white", fg="green", font=("helvetica", 20))
+lb_titulo.place(x=240, y=10)
+
+# Etiqueta para el primer número 
+lb_a = Label(frame_entrada, text="a :")
+lb_a.config(bg="white", fg="green", font=("helvetica", 20))
+lb_a.place(x=250, y=60)
+
+# Caja de texto (entry) para el primer número
+entry_a = Entry(frame_entrada, textvariable=a)
+entry_a.config(bg="white", fg="green", font=("courier", 20))
+entry_a.focus_set()
+entry_a.place(x=290, y=60, width=100, height=30)
+
+# Etiqueta para el segundo número 
+lb_b = Label(frame_entrada, text="b :")
+lb_b.config(bg="white", fg="green", font=("helvetica", 20))
+lb_b.place(x=250, y=100)
+
+# Caja de texto (entry) para el segundo número
+entry_b = Entry(frame_entrada, textvariable=b)
+entry_b.config(bg="white", fg="green", font=("courier", 20))
+entry_b.place(x=290, y=100, width=100, height=30)
 # ---------------------
 # Frame operaciones
 # ---------------------
 frame_operaciones = Frame(ventana_principal)
-frame_operaciones.config(bg="blue",width=500, height=125)
-frame_operaciones.place(x=0,y=250)
+frame_operaciones.config(bg="white",width=480, height=100)
+frame_operaciones.place(x=10,y=200)
+
+# Botón para calcular 
+bt_calcular = Button(frame_operaciones, text="Calcular", command=calcular)
+bt_calcular.place(x=45, y=35, width=100, height=30)
+
+# Botón para Borrar 
+bt_Borrar = Button(frame_operaciones, text="Borrar", command=borrar)
+bt_Borrar.place(x=190, y=35, width=100, height=30)
+
+# Botón para Salir 
+bt_Salir = Button(frame_operaciones, text="Salir", command=salir)
+bt_Salir.place(x=335, y=35, width=100, height=30)
+
 
 # ---------------------
 # Frame resultados
 # ---------------------
 frame_resultados = Frame(ventana_principal)
-frame_resultados.config(bg="red",width=500, height=125)
-frame_resultados.place(x=0,y=375)
+frame_resultados.config(bg="white",width=480, height=180)
+frame_resultados.place(x=10,y=310)
+
+# Area de texto para resultados
+t_resultados = Text(frame_resultados)
+t_resultados.config(bg="black", fg="green", font=("courier", 20))
+t_resultados.place(x=10, y=10, width=460, height=160)
 
 # Run
 # Se ejecuta la función (metodo) mainloop de la clase Tk() a traves de la instancia ventana_principal. Este
